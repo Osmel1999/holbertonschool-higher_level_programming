3c403bb3d7ecfcb661b0f0c85594f34dea83d89c
osmel <odfarakm@gmail.com>
2020-05-07 00:17:24 -0500

    reserve word changed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,9 +3,9 @@ def divisible_by_2(my_list=[]):
         new_list [len(my_list)]
         for i in range(len(my_list)):
             if my_list[i] == 0:
-                new_list.insert(i, False)
-            elif my_list[i]%2 == 0:
-                    new_list.insert(i, True)
+                new_list.append(False)
+            elif my_list[i] % 2 == 0:
+                    new_list.append(True)
             else:
-                new_list.insert(i, False)
+                new_list.append(False)
         return new_list
