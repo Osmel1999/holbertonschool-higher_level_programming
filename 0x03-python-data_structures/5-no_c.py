7dfc879446e36ac9b146e8cb0eaed60943c547dc
osmel <odfarakm@gmail.com>
2020-05-06 21:25:05 -0500

    first test

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in range (len(my_string=[])):
+        b = my_string[i]
+        new_string []
+        if b == "c" or b == "C":
+            del my_string[i]
+        else:
+            new_string.insert(my_string[i])
+    return new_string
+
