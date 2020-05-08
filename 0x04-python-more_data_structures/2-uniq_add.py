4960655a45e605b4823b5619767d16d6ca794c85
osmel <odfarakm@gmail.com>
2020-05-07 21:47:06 -0500

    second try

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    result = []
-    for uno in range(len(my_list)):
-        for dos in range(len(my_list)):
-            if my_list[uno] == my_list[dos] and uno != dos:
-                result.append(my_list[uno])
-        return result
-
-
+    new = [set(my_list)]
+    return new
