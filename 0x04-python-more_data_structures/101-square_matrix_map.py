ebd54e4468443d1413f4a377f937f8e423380db3
osmel <odfarakm@gmail.com>
2020-05-08 00:20:19 -0500

    next try

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def square_cal(n):
-    return n ** 2
 def square_matrix_map(matrix=[]):
-    new_matrix = [map(square_cal, matrix)]
+    return list(map(lambda x: list(map(lambda i: i ** 2, x)), matrix))
