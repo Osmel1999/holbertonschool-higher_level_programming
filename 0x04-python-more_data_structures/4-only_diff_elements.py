2f7f358b95943336110225368752aed0a91d48a6
osmel <odfarakm@gmail.com>
2020-05-07 22:58:10 -0500

    first try

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    my_list = []
-    my_list = set_1 - set_2
-    my_list.append(set_2 - set_1)
-    return my_list
+    return set_1 ^ set_2
