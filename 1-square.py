afa54b6f958d26af87e0e742bd21ae4299212f02
osmel <odfarakm@gmail.com>
2020-05-19 23:15:09 -0500

    nEw2

--- 1-square.py
+++ 1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
 
+''' Square form '''
+
+
+class Square:
+    ''' Class with size attribute '''
     def __init__(self, size):
-        """Initializes the data."""
         self.__size = size
