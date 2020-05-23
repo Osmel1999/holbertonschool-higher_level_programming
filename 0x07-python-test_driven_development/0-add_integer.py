2e159292090ba5b312ceb4961b176349e3762db0
osmel <odfarakm@gmail.com>
2020-05-23 14:22:41 -0500

    new fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,15 +5,16 @@ Adds two integer together
 
 """
 
-def add_integer (a, b=98):
-    """Returns the addition of a and b, or 
-    and error if a and b are not integers or floats
+
+def add_integer(a, b=98):
+    """Returns the addition of a and b,
+    or an error if a and b are not integers or floats
     """
 
     if type(a) is not int and type(a) is not float:
-        raise TypeError("most be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int and type(b) is not float:
-        raise TypeError("most be an integer")
+        raise TypeError("b must be an integer")
 
     if type(a) is float:
         a = int(a)
--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,7 @@ Module say_my_name
 Prints a given first name and last name.
 """
 
+
 def say_my_name(first_name, last_name=""):
     """Prints a string with <first_name>
     and <last_name>.
@@ -14,5 +15,5 @@ def say_my_name(first_name, last_name=""):
 
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-    
+
     print("My name is {} {}".format(first_name, last_name))
