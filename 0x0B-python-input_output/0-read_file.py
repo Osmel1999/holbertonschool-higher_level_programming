42dbfd9248dc35238a2758c9f20b3e2a53709c32
osmel <odfarakm@gmail.com>
2020-06-03 20:48:49 -0500

    new

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+""" Module 0-read_file.
+Reads from a file and prints
+"""
+
 def read_file(filename=""):
     with open(filename, 'r') as f:
         read_data = f.read()
-    f.closed
-return read_data
+        print(read_data, end="")
