9c431cfa678724d871b42ecbe472c3e4111a0192
osmel <odfarakm@gmail.com>
2020-06-03 22:27:44 -0500

    fixing comment things

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -2,12 +2,14 @@
 """Module 2-read_lines.
 Reads a certain number of lines from a file.
 """
+
+
 def write_file(filename="", text=""):
     """Writes text in filename.
 
     Args:
-      - filename: name of the file
-      - text: the text that gonna be added to the file
+        - filename: name of the file
+        - text: the text that gonna be added to the file
 
       Retuns: number of characters written
     """
