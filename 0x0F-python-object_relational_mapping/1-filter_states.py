445860a5768d99a459b476ad121fd273123e35cc
Osmel <odfarakm@gmail.com>
2020-12-14 02:13:18 +0000

    new test

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+i!/usr/bin/python3
 """list all states with its names start with N (Uppercase N)"""
 
 if __name__ = '__main__':
@@ -8,10 +8,8 @@ if __name__ = '__main__':
 	db = MySLQdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argsv[2], db=sys.argsv[3])
 
 	c = db.cursor()
-	c = execute("SELECT * FROM states ORDER by states.id ASC;")
-	rows = cur.fetchall()
+	c = execute("""SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER by states.id ASC;""")
+	rows = c.fetchall()
 	for row in rows:
-		for ch in row:
-			if ch is N:
-				print(row)
+		print(row)
  
